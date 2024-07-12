b4fd12b96f1e05a1ca16e306cfd4ad4f37a97821
1heeheeheeheeheheeheeheehee <saeedmansoor409@gmail.com>
2024-07-12 11:08:09 +0400

    Update Main.lua

--- src/Main.lua
+++ src/Main.lua
@@ -6,7 +6,7 @@ local __index = getrawmetatable(game).__index
 repeat task.wait() until game["Players"]
 
 if not select(2, pcall(__index, game.Players, "LocalPlayer")) then
-	local Success, Unload = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub-V2/main/src/Modules/Original.lua")))) -- Original / best version
+	local Success, Unload = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/1heeheeheeheeheheeheeheehee/vex/main/src/Modules/Original.lua")))) -- Original / best version
 	
 	if not Success then
 		warn("AIRHUB_V2 > Loader - Your script execution software does not support the original / best version, trying the degraded version...")
@@ -17,14 +17,14 @@ if not select(2, pcall(__index, game.Players, "LocalPlayer")) then
 			Unload()
 		end
 		
-		Success = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub-V2/main/src/Modules/Primitive.lua")))) -- Degraded for UWP (works on web version)
+		Success = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/1heeheeheeheeheheeheeheehee/vex/main/src/Modules/Primitive.lua")))) -- Degraded for UWP (works on web version)
 	
 		if not Success then
 			return warn("AIRHUB_V2 > Loader - Your script execution software does not support this script.")
 		end
 	end
 else
-	local Success = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub-V2/main/src/Modules/Primitive.lua")))) -- Degraded for UWP (works on web version)
+	local Success = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/1heeheeheeheeheheeheeheehee/vex/main/src/Modules/Primitive.lua")))) -- Degraded for UWP (works on web version)
 	
 	if not Success then
 		return warn("AIRHUB_V2 > Loader - Your script execution software does not support this script.")
